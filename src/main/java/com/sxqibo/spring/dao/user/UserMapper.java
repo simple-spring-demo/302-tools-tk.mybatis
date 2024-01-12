1a952c4832d8dbfbaa017a752670ba154ac9e5f7
YangHongwei <403236160@qq.com>
2024-01-12 17:31:00 +0800

    feat: 自动生成用户相关

---
+++ src/main/java/com/sxqibo/spring/dao/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.sxqibo.spring.dao.user;
+
+import com.sxqibo.spring.domain.entity.user.User;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<User> {
+}
