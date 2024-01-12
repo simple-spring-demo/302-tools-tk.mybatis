74bde45fb82402c27f7dbdc23073b436bb1cefb2
YangHongwei <403236160@qq.com>
2024-01-12 17:17:14 +0800

    feat: 增加注解

--- src/main/java/com/sxqibo/spring/Application.java
+++ src/main/java/com/sxqibo/spring/Application.java
@@ -3,6 +3,7 @@ package com.sxqibo.spring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@tk.mybatis.spring.annotation.MapperScan(basePackages = "com.sxqibo")
 @SpringBootApplication
 public class Application {
 
